26e5c15a780a888486bd08014c3621345c0fa140
Trond Boksasp <trondboksasp@gmail.com>
2020-02-10 20:15:20 +0100

    Remove build output folder from git

--- out/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEST_OR_SUBJECT_NOT_FOUND = 'Could\'t find test/test subject';
-exports.ACTIVE_TEXT_EDITOR_UNDEFINED = 'No editor currently active';
-//# sourceMappingURL=constants.js.map
